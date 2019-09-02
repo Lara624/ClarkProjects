638dc2a675350564d5900b915742096f50d880aa
Ayobami <Ayobami.Elutade@outlook.com>
2019-09-02 15:14:37 +0100

    first push

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -1,9 +1,9 @@
 package cucumberOptions;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+//import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 //@RunWith(Cucumber.class)
 @CucumberOptions(
